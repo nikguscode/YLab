edaa9feb9278dc6860226c28c8c3739954d69744
nikguscode <109280336+nikguscode@users.noreply.github.com>
2024-10-15 01:10:07 +0500

    исправления PR

---
+++ src/main/java/infrastructure/dao/habit/LocalHabitDao.java
@@ -0,0 +1,28 @@
+package infrastructure.dao.habit;
+
+import core.entity.Habit;
+
+import java.util.UUID;
+
+public class LocalHabitDao implements HabitDao {
+    @Override
+    public void add(Habit habit) {
+        // for jdbc implementation
+    }
+
+    @Override
+    public Habit get(UUID id) {
+        // for jdbc implementation
+        return null;
+    }
+
+    @Override
+    public void delete(Habit habit) {
+        // for jdbc implementation
+    }
+
+    @Override
+    public void edit(Habit habit) {
+        // for jdbc implementation
+    }
+}
