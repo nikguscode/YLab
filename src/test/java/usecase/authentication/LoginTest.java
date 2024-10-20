da8a996c0cfa78081bce991287dc85a33c49766b
nikguscode <109280336+nikguscode@users.noreply.github.com>
2024-10-20 23:47:07 +0500

    refactor: переделан для JDBC

--- src/test/java/usecase/authentication/LoginTest.java
+++ src/test/java/usecase/authentication/LoginTest.java
@@ -1,76 +1,117 @@
-//package usecase.authentication;
-//
-//import core.entity.User;
-//import core.exceptions.InvalidUserInformationException;
-//import infrastructure.dao.user.LocalUserDao;
-//import infrastructure.dao.user.UserDao;
-//import infrastructure.dto.LoginDto;
-//import org.assertj.core.api.Assertions;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import usecase.authentication.login.LocalLogin;
-//import usecase.authentication.login.Login;
-//
-//public class LoginTest {
-//    private final Login login;
-//
-//    public LoginTest() throws InvalidUserInformationException {
-//        login = new LocalLogin();
-//        UserDao userDao = new LocalUserDao();
-//
-//        userDao.add(User.builder()
-//                .email("correct@gmail.com")
-//                .username("some username")
-//                .password("1")
-//                .build()
-//        );
-//    }
-//
-//    @Test
-//    @DisplayName("Login With Correct Data")
-//    public void login_With_Correct_Data() {
-//        boolean result = login.isSuccess(
-//                LoginDto.builder()
-//                .email("correct@gmail.com")
-//                .password("1")
-//                .build()
-//        );
-//        Assertions.assertThat(result).isTrue();
-//    }
-//
-//    @Test
-//    @DisplayName("Login If Email Doesn't Exists")
-//    public void login_If_Email_Not_Exists() {
-//        boolean result = login.isSuccess(
-//                LoginDto.builder()
-//                        .email("notExistsEmail@gmail.com")
-//                        .password("1")
-//                        .build()
-//        );
-//        Assertions.assertThat(result).isFalse();
-//    }
-//
-//    @Test
-//    @DisplayName("Login With Incorrect Password")
-//    public void login_With_Incorrect_Password() {
-//        boolean result = login.isSuccess(
-//                LoginDto.builder()
-//                        .email("correct@gmail.com")
-//                        .password("incorrectPassword")
-//                        .build()
-//        );
-//        Assertions.assertThat(result).isFalse();
-//    }
-//
-//    @Test
-//    @DisplayName("Login With Blank Data")
-//    public void login_With_Blank_Data() {
-//        boolean result = login.isSuccess(
-//                LoginDto.builder()
-//                        .email("")
-//                        .password("")
-//                        .build()
-//        );
-//        Assertions.assertThat(result).isFalse();
-//    }
-//}
+package usecase.authentication;
+
+import core.entity.User;
+import core.enumiration.Role;
+import core.exceptions.InvalidUserInformationException;
+import infrastructure.DatabaseUtils;
+import infrastructure.configuration.LiquibaseMigration;
+import infrastructure.dao.user.JdbcUserDao;
+import infrastructure.dao.user.UserDao;
+import infrastructure.dto.LoginDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.*;
+import org.testcontainers.containers.PostgreSQLContainer;
+import usecase.authentication.login.JdbcLogin;
+import usecase.authentication.login.Login;
+
+import java.time.LocalDateTime;
+
+public class LoginTest {
+    public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
+    private Login login;
+    private UserDao userDao;
+    private User user;
+
+    @BeforeAll
+    public static void beforeAll() {
+        postgres.start();
+        new LiquibaseMigration(
+                postgres.getJdbcUrl(),
+                postgres.getUsername(),
+                postgres.getPassword()
+        ).migrateDatabase();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        postgres.stop();
+    }
+
+
+    @BeforeEach
+    public void setUp() throws InvalidUserInformationException {
+        DatabaseUtils databaseUtils = new DatabaseUtils(
+                "org.postgresql.Driver",
+                postgres.getJdbcUrl(),
+                postgres.getUsername(),
+                postgres.getPassword()
+        );
+
+        this.login = new JdbcLogin(new JdbcUserDao(databaseUtils));
+        this.userDao = new JdbcUserDao(databaseUtils);
+        this.user = User.builder()
+                .email("test@gmail.com")
+                .username("testuser")
+                .password("root")
+                .role(Role.ADMINISTRATOR)
+                .registrationDate(LocalDateTime.now())
+                .authorizationDate(LocalDateTime.now())
+                .build();
+
+        userDao.add(user);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        userDao.delete(user);
+        user = null;
+    }
+
+    @Test
+    @DisplayName("Login With Correct Data")
+    public void login_With_Correct_Data() {
+        boolean result = login.isSuccess(
+                LoginDto.builder()
+                        .email("test@gmail.com")
+                        .password("root")
+                        .build()
+        );
+        Assertions.assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("Login If Email Doesn't Exist")
+    public void login_If_Email_Not_Exists() {
+        boolean result = login.isSuccess(
+                LoginDto.builder()
+                        .email("notExistsEmail@gmail.com")
+                        .password("1")
+                        .build()
+        );
+        Assertions.assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("Login With Incorrect Password")
+    public void login_With_Incorrect_Password() {
+        boolean result = login.isSuccess(
+                LoginDto.builder()
+                        .email("admin@gmail.com")
+                        .password("incorrectPassword")
+                        .build()
+        );
+        Assertions.assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("Login With Blank Data")
+    public void login_With_Blank_Data() {
+        boolean result = login.isSuccess(
+                LoginDto.builder()
+                        .email("")
+                        .password("")
+                        .build()
+        );
+        Assertions.assertThat(result).isFalse();
+    }
+}
